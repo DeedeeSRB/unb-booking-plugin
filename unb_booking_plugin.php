a0cd8dc88cbce078ef490d71e9ad8469bc4402cc
Sadik Besata <sadik.besata@agu.edu.tr>
2022-07-29 14:53:06 +0300

    inital plugin header

---
+++ unb_booking_plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package UNBBookingPlugin
+ */
+
+ /*
+ Plugin Name: Unbelievable Digital Booking Plugin
+ Description: First implimentation of Unbelievable Digital booking system
+ Version: 1.0.0
+ Author: Unbelievable Digital
+ Author URI: https://unbelievable.digital/
+ License: GPLv2 or later 
+ Text Domain: unb-booking-plugin
+ */
