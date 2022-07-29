a5736aad447ecefedd96f623ea15cdf33b2fa9bd
Moxie5 <dobromir00@gmail.com>
2022-07-29 16:19:09 +0300

    Init

--- unb_booking_plugin.php
+++ unb_booking_plugin.php
@@ -12,3 +12,9 @@
  License: GPLv2 or later 
  Text Domain: unb-booking-plugin
  */
+define( 'UNB_BOOKING', __FILE__ );
+/**
+ * Include the Elementor_Awesomesauce class.
+ */
+require plugin_dir_path( UNB_BOOKING ) . 'class-unb-booking-system.php';
+
---
+++ widgets/class-main.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * UnbBooking class.
+ *
+ * @category   Class
+ * @package    UnbBooking
+ * @subpackage WordPress
+ * @author     Unbelievable Digital
+ * @copyright  2022 Unbelievable Digital
+ * @license    https://opensource.org/licenses/GPL-3.0 GPL-3.0-only
+ * @link       https://unbelievable.digital/
+ * @since      1.0.0
+ * php version 7.3.9
+ */
+namespace UnbBooking\Widgets;
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+// Security Note: Blocks direct access to the plugin PHP files.
+defined( 'ABSPATH' ) || die();
+/**
+ * Awesomesauce widget class.
+ *
+ * @since 1.0.0
+ */
+class UnbBooking extends Widget_Base {
+	/**
+	 * Class constructor.
+	 *
+	 * @param array $data Widget data.
+	 * @param array $args Widget arguments.
+	 */
+	public function __construct( $data = array(), $args = null ) {
+		parent::__construct( $data, $args );
+		wp_register_style( 'unb_booking_style', plugins_url( '/assets/css/main.css', UNB_BOOKING ), array(), '1.0.0' );
+		wp_register_script( 'unb_booking_script', plugins_url( '/assets/js/script.js', UNB_BOOKING ), array('jquery'), '1.0.0' );
+	}
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'unb_booking';
+	}
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'UNB Booking', 'unb_booking' );
+	}
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'fa fa-pencil';
+	}
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return array( 'general' );
+	}
+	
+	/**
+	 * Enqueue styles.
+	 */
+	public function get_style_depends() {
+		return array( 'unb_booking_style' );
+	}
+
+	/**
+	 * Enqueue script.
+	 */
+	public function get_script_depends() {
+		return array( 'unb_booking_script' );
+	}
+
+	/**
+	 * Register the widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function _register_controls() {
+		$this->start_controls_section(
+			'section_content',
+			array(
+				'label' => __( 'UNB Booking', 'unb_booking' ),
+			)
+		);
+		$this->add_control(
+			'title',
+			array(
+				'label'   => __( 'Title', 'unb_booking' ),
+				'type'    => Controls_Manager::TEXT,
+				'default' => __( 'Title', 'unb_booking' ),
+			)
+		);
+		
+		$this->end_controls_section();
+	}
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function render() {
+		$settings = $this->get_settings_for_display();
+		
+	}
+	/**
+	 * Render the widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function _content_template() {
+		
+	}
+}
