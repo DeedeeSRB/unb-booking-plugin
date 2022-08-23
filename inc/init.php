8f15470a861f5db3ceb5a348a6c1262cf9b9f372
Sadik Besata <sadik.bsata89@gmail.com>
2022-08-23 12:02:22 +0300

    init.php comments

--- inc/init.php
+++ inc/init.php
@@ -1,6 +1,10 @@
 <?php
 /**
- * @package  UNBBookingPlugin
+ * Responsible to initiate all the UNB classes by calling their register methods.
+ *
+ * @category   Class
+ * @package    UNBBookingPlugin\Classes
+ * @since      1.0.0
  */
 namespace UnbBooking;
 
@@ -11,20 +15,25 @@ use UnbBooking\Manage\AjaxManager;
 use UnbBooking\Manage\WCManager;
 use UnbBooking\CPTs\CustomRegisterCPT;
 
+ /**
+ * Initiator class
+ */
 final class Init
 {
 	/**
 	 * Store all the classes inside an array
+	 * 
+	 * @since 1.0.0
 	 * @return array Full list of classes
 	 */
 	public static function get_services() 
 	{
-		require plugin_dir_path( UNB_BOOKING ) . '/inc/Base/Enqueue.php';
-		require plugin_dir_path( UNB_BOOKING ) . '/inc/Admin/Admin.php';
-		require plugin_dir_path( UNB_BOOKING ) . '/inc/Admin/CPTAdmin.php';
-		require plugin_dir_path( UNB_BOOKING ) . '/inc/Manage/AjaxManager.php';
-		require plugin_dir_path( UNB_BOOKING ) . '/inc/Manage/WCManager.php';
-		require plugin_dir_path( UNB_BOOKING ) . '/inc/CPT/CustomRegisterCPT.php';
+		require_once plugin_dir_path( UNB_BOOKING ) . '/inc/Base/Enqueue.php';
+		require_once plugin_dir_path( UNB_BOOKING ) . '/inc/Admin/Admin.php';
+		require_once plugin_dir_path( UNB_BOOKING ) . '/inc/Admin/CPTAdmin.php';
+		require_once plugin_dir_path( UNB_BOOKING ) . '/inc/Manage/AjaxManager.php';
+		require_once plugin_dir_path( UNB_BOOKING ) . '/inc/Manage/WCManager.php';
+		require_once plugin_dir_path( UNB_BOOKING ) . '/inc/CPT/CustomRegisterCPT.php';
 
 		return [
 			Enqueue::class,
@@ -39,7 +48,8 @@ final class Init
 	/**
 	 * Loop through the classes, initialize them, 
 	 * and call the register() method if it exists
-	 * @return
+	 * 
+	 * @since 1.0.0
 	 */
 	public static function register_services() 
 	{
